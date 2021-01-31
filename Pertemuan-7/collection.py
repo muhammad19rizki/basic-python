7475b949251cce74f5dbb55ddb25e00d89ab56c6
muhammad19rizki <muhammad1rizki9@gmail.com>
2021-01-31 12:45:41 +0700

    add pertemuan 7

---
+++ Pertemuan-7/collection.py
@@ -0,0 +1,23 @@
+# tuple
+
+myTuple = ("Jakarta", "Tangerang", "Bekasi", "Bogor", "Depok")
+print(myTuple[1])
+dt=myTuple[4]
+print(dt)
+
+# Sets
+
+mySets = {"Pizza", "Nasi Padang", "Bakso", "Mie Ayam", "Sate Ayam"}
+print(mySets)
+
+#Lopp sets
+for x in mySets:
+    print(x)
+
+#ADD sets
+mySets.add("Sate Kambing")
+print(mySets)
+
+#Remove sets
+mySets.remove("Pizza")
+print(mySets)
